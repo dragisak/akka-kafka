f0ebfab4f61e2e0143ab9d24008af5784f4e9c14
Dragisa Krsmanovic <dragisa@crunchbase.com>
2021-11-12 12:30:33 -0800

    WIP

--- src/main/scala/dragisa/kafka/ConfluentKafkaSerde.scala
+++ src/main/scala/dragisa/kafka/ConfluentKafkaSerde.scala
@@ -6,12 +6,6 @@ import com.sksamuel.avro4s.kafka.GenericSerde
 /** See https://docs.confluent.io/platform/current/schema-registry/serdes-develop/index.html#wire-format
   */
 class ConfluentKafkaSerde[T >: Null: SchemaFor: Encoder: Decoder] extends GenericSerde[T](BinaryFormat) {
-  override def deserialize(topic: String, data: Array[Byte]): T = {
-    if (data == null) {
-      null
-    } else
-      super.deserialize(topic, data.drop(5))
-  }
 
   private val dummyPrefix: Array[Byte] = Array(
     0, // MagicNumber always 0
@@ -20,6 +14,12 @@ class ConfluentKafkaSerde[T >: Null: SchemaFor: Encoder: Decoder] extends Generi
     0,
     0
   )
+
+  override def deserialize(topic: String, data: Array[Byte]): T = if (data == null) {
+    null
+  } else {
+    super.deserialize(topic, data.drop(5))
+  }
 
   override def serialize(topic: String, data: T): Array[Byte] = {
     val bytes = super.serialize(topic, data)
