25f50f747fd1b100f93bfdf8c57e6ad9da9266ea
Dragisa Krsmanovic <dragisa@crunchbase.com>
2021-11-12 09:19:30 -0800

    WIP

--- src/main/scala/dragisa/kafka/FacetKey.scala
+++ src/main/scala/dragisa/kafka/FacetKey.scala
@@ -7,6 +7,33 @@ import org.apache.kafka.common.serialization.Serde
 
 import java.util.UUID
 
+/** {{{
+  * {
+  *   "type": "record",
+  *   "name": "Key",
+  *   "namespace": "entityservice.848.public.node_facets_v2",
+  *   "fields": [
+  *     {
+  *       "name": "entity_def_id",
+  *       "type": "string"
+  *     },
+  *     {
+  *       "name": "facet_id",
+  *       "type": "string"
+  *     },
+  *     {
+  *       "name": "uuid",
+  *       "type": {
+  *         "type": "string",
+  *         "connect.version": 1,
+  *         "connect.name": "io.debezium.data.Uuid"
+  *       }
+  *     }
+  *   ],
+  *   "connect.name": "entityservice.848.public.node_facets_v2.Key"
+  * }
+  * }}}
+  */
 final case class FacetKey(
     entityDefId: String,
     facetId: String,
--- src/main/scala/dragisa/kafka/FacetValue.scala
+++ src/main/scala/dragisa/kafka/FacetValue.scala
@@ -9,6 +9,62 @@ import org.apache.kafka.common.serialization.Serde
 import java.time.ZonedDateTime
 import java.util.UUID
 
+/** {{{
+  * {
+  *   "type": "record",
+  *   "name": "Value",
+  *   "namespace": "entityservice.848.public.node_facets_v2",
+  *   "fields": [
+  *     {
+  *       "name": "entity_def_id",
+  *       "type": "string"
+  *     },
+  *     {
+  *       "name": "facet_id",
+  *       "type": "string"
+  *     },
+  *     {
+  *       "name": "uuid",
+  *       "type": {
+  *         "type": "string",
+  *         "connect.version": 1,
+  *         "connect.name": "io.debezium.data.Uuid"
+  *       }
+  *     },
+  *     {
+  *       "name": "properties",
+  *       "type": [
+  *         "null",
+  *         {
+  *           "type": "string",
+  *           "connect.version": 1,
+  *           "connect.name": "io.debezium.data.Json"
+  *         }
+  *       ],
+  *       "default": null
+  *     },
+  *     {
+  *       "name": "created_at",
+  *       "type": {
+  *         "type": "string",
+  *         "connect.version": 1,
+  *         "connect.name": "io.debezium.time.ZonedTimestamp"
+  *       }
+  *     },
+  *     {
+  *       "name": "updated_at",
+  *       "type": {
+  *         "type": "string",
+  *         "connect.version": 1,
+  *         "connect.name": "io.debezium.time.ZonedTimestamp"
+  *       }
+  *     }
+  *   ],
+  *   "connect.name": "entityservice.848.public.node_facets_v2.Value"
+  * }
+  * }}}
+  */
+
 final case class FacetValue(
     entityDefId: String,
     facetId: String,
